2644ebea48de7a58baf34a470a0bafeb56849230
AlenBG <alen.pechovski@abv.bg>
2021-01-30 22:21:29 +0200

    commit

---
+++ strings.exs
@@ -0,0 +1,11 @@
+defmodule F do
+  def operations do
+    string = "I love"
+    longer_string = string <> " " <> "coding"
+    IO.puts "Lenght : #{String.length(string)}"
+    IO.puts "Lenght : @{String.lenght(longer_string)}"
+    IO.puts "Contain coding? #{String.contains?(string, "coding")}"
+    IO.puts "Contain coding? #{String.contains?(longer_string, "coding")}"
+    Io.puts "First - #{String.first(string)}"
+  end
+end
